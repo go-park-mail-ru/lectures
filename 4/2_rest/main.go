cfe531aaf4368fe49f33ba80d30e798552765404
Vasily Romanov <romanov.vasily@mail.ru>
2019-09-25 18:02:59 +0300

    4

--- 4/2_rest/main.go
+++ 4/2_rest/main.go
@@ -54,6 +54,38 @@ func (api *BooksHandler) Add(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Result{Body: body})
 }
 
+func (api *BooksHandler) BookByID(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, `{"error":"bad id"}`, 400)
+		return
+	}
+
+	books, err := api.store.GetBooks()
+	if err != nil {
+		http.Error(w, `{"error":"db"}`, 500)
+		return
+	}
+
+	var book *Book
+	for _, b := range books {
+		if b.ID == uint(id) {
+			book = b
+			break
+		}
+	}
+	if book == nil {
+		http.Error(w, `{"body":{"book":null}}`, 404)
+		return
+	}
+
+	body := map[string]interface{}{
+		"book": book,
+	}
+	json.NewEncoder(w).Encode(&Result{Body: body})
+}
+
 func main() {
 	r := mux.NewRouter()
 
@@ -63,6 +95,7 @@ func main() {
 
 	r.HandleFunc("/", api.List)
 	r.HandleFunc("/add", api.Add)
+	r.HandleFunc("/book/{id:[0-9]+}", api.BookByID)
 
 	log.Println("start serving :8080")
 	http.ListenAndServe(":8080", r)
