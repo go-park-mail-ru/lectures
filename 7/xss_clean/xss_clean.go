bf86016be692fe40eac413db47c06b333b7dcc4e
Vasily Romanov <romanov.vasily@mail.ru>
2019-10-23 10:41:42 +0300

    7

---
+++ 7/xss_clean/xss_clean.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/microcosm-cc/bluemonday"
+)
+
+// для санитайзинга на сторое фронта используйте https://github.com/cure53/DOMPurify
+
+func main() {
+	sanitizer := bluemonday.UGCPolicy()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		comment := `<a onblur="alert(document.сookie)" href="https://www.mail.ru">Mail.ru</a>`
+		comment = sanitizer.Sanitize(comment)
+		resp, _ := json.Marshal(map[string]interface{}{
+			"comment": comment,
+		})
+		w.Write(resp)
+	})
+
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
+}
