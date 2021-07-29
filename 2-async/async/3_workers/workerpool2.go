f66095bb70bb68f48e1978c4ea979cf91ab5da87
Muradyan Artem <muradyanartem2@gmail.com>
2021-07-29 16:09:04 +0300

    Create workerpool2.go

---
+++ 2-async/async/3_workers/workerpool2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Task struct {
+	in  int
+	out *int
+}
+
+func worker(input <-chan Task) {
+	for t := range input {
+		// Long compute operation simulation
+		time.Sleep(100 * time.Millisecond)
+		*t.out = t.in * 2
+	}
+}
+
+func main() {
+	const (
+		Workers  = 10
+		Sequence = 1000
+	)
+
+	var wg sync.WaitGroup
+	wg.Add(Workers)
+
+	// Buffer equal to the size of workers will allow utilize workers as much as possible
+	ch := make(chan Task, Workers)
+
+	// Creates workerpool
+	for i := 0; i < Workers; i++ {
+		go func() {
+			defer wg.Done()
+			worker(ch)
+		}()
+	}
+
+	res := make([]int, Sequence)
+
+	// Produces tasks
+	for i := 0; i < Sequence; i++ {
+		ch <- Task{in: i, out: &res[i]}
+	}
+
+	close(ch)
+
+	wg.Wait()
+
+	// Prints sequence of tasks result
+	for _, val := range res {
+		fmt.Println(val)
+	}
+}
